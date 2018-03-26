8f6c3ca23d8a7f4a11e32ff9e8b5efa6d66b678c
Matthijs Knigge <matthijsknigge@hotmail.com>
2018-03-26 14:20:42 +0200

    Better comments in function

--- elastic.net.regularization/R/glmnet.TMM.normalize.R
+++ elastic.net.regularization/R/glmnet.TMM.normalize.R
@@ -4,38 +4,30 @@
 #' @export
 #' @examples
 #' glmnet.TMM.normalize()
-glmnet.TMM.normalize <- function() {
-  args <- commandArgs(TRUE)
-  
+glmnet.TMM.normalize <- function(raw.cell.counts, output.file) {
+  # libraries
   require(edgeR)
   require(data.table)
-  install.packages("edgeR")
+
   
-  countsFile=args[1]
-  normOut=args[2]
+  countsFile=raw.cell.counts
+  normOut=output.file
   
+  # pre parse table to apptropriate format
   table           <- fread(countsFile, data.table=F)
   rownames(table) <- table[,1]
   table           <- table[,-1]
   D               <- DGEList(counts=table)
-  
-  cat("[INFO] ", nrow(D), " genes in input\n")
-  cat("[INFO] ", ncol(D), " samples in input\n")
-  
   # Remove low expressed genes 500FG SPECIFIC
   
   # Remove zero genes
   keep          <- rowSums(D$counts == 0) < 0.8 * ncol(D$counts)
   D             <- D[keep, , keep.lib.sizes=FALSE]
-  cat("[INFO] Removed genes with more then 80% zeroes.\n")
-  
+
   # Remove low varying genes
   var           <- apply(D$counts, 1, var)
   keep          <- var > 0.1
   D             <- D[keep, , keep.lib.sizes=FALSE]
-  
-  cat("[INFO] ", nrow(D), " genes remaining after filtering.\n")
-  
   
   # TMM normilization
   d             <- calcNormFactors(D)
@@ -44,8 +36,6 @@ glmnet.TMM.normalize <- function() {
   scaled.counts <- d$counts/scal.mat
   
   write.table(t(scaled.counts), file = normOut, sep = "\t", row.names=TRUE, quote=FALSE, col.names = NA)
-  cat("[INFO] Output written.\n")
-  
 }
 
 

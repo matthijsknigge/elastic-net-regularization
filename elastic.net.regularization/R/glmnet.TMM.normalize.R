c989fe159b4a55e72dee0117b4128c6bc866a6a7
Matthijs Knigge <matthijsknigge@hotmail.com>
2018-03-28 11:18:58 +0200

    Added comments

--- elastic.net.regularization/R/glmnet.TMM.normalize.R
+++ elastic.net.regularization/R/glmnet.TMM.normalize.R
@@ -13,17 +13,17 @@ glmnet.TMM.normalize <- function(raw.cell.counts, output.file) {
   require(edgeR)
   require(data.table)
 
-  
+
   countsFile=raw.cell.counts
   normOut=output.file
-  
+
   # pre parse table to apptropriate format
   table           <- fread(countsFile, data.table=F)
   rownames(table) <- table[,1]
   table           <- table[,-1]
   D               <- DGEList(counts=table)
   # Remove low expressed genes 500FG SPECIFIC
-  
+
   # Remove zero genes
   keep          <- rowSums(D$counts == 0) < 0.8 * ncol(D$counts)
   D             <- D[keep, , keep.lib.sizes=FALSE]
@@ -32,13 +32,13 @@ glmnet.TMM.normalize <- function(raw.cell.counts, output.file) {
   var           <- apply(D$counts, 1, var)
   keep          <- var > 0.1
   D             <- D[keep, , keep.lib.sizes=FALSE]
-  
+
   # TMM normilization
   d             <- calcNormFactors(D)
   scalar        <- d$samples$lib.size*d$samples$norm.factors/exp(mean(log(d$samples$lib.size*d$samples$norm.factors)))
   scal.mat      <- outer(rep(1,nrow(d$counts)), scalar)
   scaled.counts <- d$counts/scal.mat
-  
+
   write.table(t(scaled.counts), file = normOut, sep = "\t", row.names=TRUE, quote=FALSE, col.names = NA)
 }
 
@@ -49,10 +49,3 @@ glmnet.TMM.normalize <- function(raw.cell.counts, output.file) {
 
 
 
-
-
-
-
-
-
-fwrite(col.names = T, row.names = F)

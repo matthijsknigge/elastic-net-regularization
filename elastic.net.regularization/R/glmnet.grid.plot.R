b6dc3f99a36cc8b5eff9798971f37b8c990085ac
Matthijs Knigge <matthijsknigge@hotmail.com>
2018-03-28 11:19:28 +0200

    Function was not flexible, now is.

--- elastic.net.regularization/R/glmnet.grid.plot.R
+++ elastic.net.regularization/R/glmnet.grid.plot.R
@@ -1,9 +1,7 @@
 #' plot elastic net
 #' @author Matthijs Knigge
 #'
-#' @param p.1 ggplot object
-#' @param p.2 ggplot object
-#' @param p.3 ggplot object
+#' @param plot.list list, ggplot objects
 #' @param x.lab vector character names of x-axis
 #' @param title title of plot character
 #' @param save boolean, save plot. default FASLE
@@ -18,7 +16,7 @@
 #' @return ggplot object
 #'
 #'
-glmnet.grid.plot <- function(p.1, p.2, p.3, p.4, p.5, x.lab, title, save = FALSE, return = FALSE, path, width = 10, height = 10){
+glmnet.grid.plot <- function(plot.list, x.lab, title, save = FALSE, return = FALSE, path, width = 10, height = 10){
   # libraries
   require(ggplot2)
   require(gridExtra)
@@ -27,8 +25,8 @@ glmnet.grid.plot <- function(p.1, p.2, p.3, p.4, p.5, x.lab, title, save = FALSE
   
   # create grid
   pm <- ggmatrix(
-    list(p.1, p.2, p.3, p.4, p.5),
-    nrow = 1, ncol = 5,
+    plot.list,
+    nrow = 1, ncol = length(plot.list),
     xAxisLabels = x.lab,
     title = title
   )
